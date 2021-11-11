b65ed87ec425f19f8466b4c9e84b69a87b7ab3a6
Bladimir <bladimirbaezt@gmail.com>
2021-11-11 10:19:32 -0400

    Initial project set-up commit

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse content-type application/json requests
+app.use(express.json());
+
+// parse content-type - application/x-www-form-urlencoded requests
+app.use(express.urlencoded({extended: true}));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the App"})
+});
+
+//require("./app/routes/player.routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+});
