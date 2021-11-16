89262ed4d2864a05d89fa35d60b25e00e25c571f
Bladimir <bladimirbaezt@gmail.com>
2021-11-16 12:42:29 -0400

    Added heroku config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "blad",
-    PASSWORD: "123456789",
-    DB: "playersdb"
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "b31703f28f2cc7",
+    PASSWORD: "a6d9e66b",
+    DB: "heroku_1f07204ba9bca3f"
   };
